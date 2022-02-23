b4310a4f39a933dd4ae3be95d5b56a32589919b9
muratkistan <muratkistan@gmail.com>
2022-02-23 12:28:20 +0300

    Created CreditRequestDto and CreditResultDto

---
+++ credit-service/src/main/java/com/muratkistan/dto/CreditResultDto.java
@@ -0,0 +1,21 @@
+package com.muratkistan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreditResultDto {
+    private boolean status;
+    private String identityNumber;
+    private Long creditScore;
+    private double creditLimit;
+
+    public CreditResultDto(boolean status) {
+        this.status = status;
+    }
+}
