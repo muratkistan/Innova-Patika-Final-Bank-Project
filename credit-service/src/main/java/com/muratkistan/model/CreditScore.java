d3833c5e8b1ddb79ae178cdd4eb595e39955f861
muratkistan <muratkistan@gmail.com>
2022-02-23 12:07:31 +0300

    Created CreditScore Entity class.

---
+++ credit-service/src/main/java/com/muratkistan/model/CreditScore.java
@@ -0,0 +1,29 @@
+package com.muratkistan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "credit_score")
+public class CreditScore {
+
+    @Id
+    private String id;
+
+    @NotNull
+    @Indexed(unique=true)
+    private String identityNumber;
+
+    @NotNull
+    private Long creditScore;
+}
