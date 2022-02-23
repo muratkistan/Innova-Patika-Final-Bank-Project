560323c0d868914b71ce5dd1c616224850551e84
muratkistan <muratkistan@gmail.com>
2022-02-23 11:57:02 +0300

    Adding @Log4j2 for logging in system

--- user-service/src/main/java/com/muratkistan/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/muratkistan/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.muratkistan.model.User;
 import com.muratkistan.repository.Userrepository;
 import com.muratkistan.service.abstracts.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,22 +13,26 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
 
     private final Userrepository userRepository;
 
     @Override
     public void addUser(User user) {
+        log.info(user  + " adding to DB");
         userRepository.save(user);
     }
 
     @Override
     public List<User> getAllUsers() {
+        log.info("get all users from DB");
         return userRepository.findAll();
     }
 
     @Override
     public Optional<User> getUserById(Long userId) {
+        log.info("Get one user from Db -> id: "+userId);
         return Optional.ofNullable(userRepository.findById(userId).orElseThrow(()->new NotFoundException("User ")));
     }
 
@@ -35,12 +40,15 @@ public class UserServiceImpl implements UserService {
     public User updateUser(Long userId, User user) {
         getUserById(userId);
         user.setId(userId);
+        log.info("Update user -> id: "+userId + " and "+ user);
         return userRepository.save(user);
     }
 
     @Override
     public boolean deleteUser(Long userId) {
         userRepository.delete(getUserById(userId).get());
+        log.info("Deleted user ->  id: "+userId );
+
         return true;
     }
 }
