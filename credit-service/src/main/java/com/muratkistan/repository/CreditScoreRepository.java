cb2401fcecc124ecf5d8c29a48c4189df1ac9b3b
muratkistan <muratkistan@gmail.com>
2022-02-24 16:08:39 +0300

    Refactoring CreditScorerepository

--- credit-service/src/main/java/com/muratkistan/repository/CreditScoreRepository.java
+++ credit-service/src/main/java/com/muratkistan/repository/CreditScoreRepository.java
@@ -4,7 +4,9 @@ import com.muratkistan.model.CreditScore;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CreditScoreRepository extends MongoRepository<CreditScore,String> {
-    CreditScore findByIdentityNumber(String identityNumber);
+    Optional<CreditScore> findByIdentityNumber(String identityNumber);
 }
