50298e3d519f1ee41609fc6775147d1b9b40501a
muratkistan <muratkistan@gmail.com>
2022-02-25 12:10:06 +0300

    Created Generic Exception Handler

---
+++ user-service/src/main/java/com/muratkistan/exception/handler/GenericExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.muratkistan.exception.handler;
+
+import com.muratkistan.error.ApiError;
+import com.muratkistan.exception.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GenericExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map> handleNotfoundException(NotFoundException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException exception){
+        Map<String,String> validationErrors = new HashMap<>();
+        ApiError error = new ApiError(400,"Validation Error","/users/add");
+        for (FieldError fieldError :  exception.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        error.setValidationErrors(validationErrors);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleDataIntegrityViolationException(DataIntegrityViolationException exception){
+        Map<String,String> validationErrors = new HashMap<>();
+        ApiError error = new ApiError(400,"User already exist","/users/add");
+
+        error.setValidationErrors(validationErrors);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
+}
