837b4d97986cd36d4c68a82ea64c9faed99147a1
muratkistan <muratkistan@gmail.com>
2022-02-25 11:42:51 +0300

    Added UserDto

---
+++ user-service/src/main/java/com/muratkistan/dto/UserDto.java
@@ -0,0 +1,33 @@
+package com.muratkistan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private Long id;
+
+
+    @NotNull
+    private String identityNumber;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private double monthlySalary;
+
+    @NotNull
+    private String phoneNumber;
+}
