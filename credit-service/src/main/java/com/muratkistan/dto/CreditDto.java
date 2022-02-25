a2fd88b19a2f4a96ff1be7cc3652711364e9ebf8
muratkistan <muratkistan@gmail.com>
2022-02-25 11:42:21 +0300

    Added CreditDto and UserDto

---
+++ credit-service/src/main/java/com/muratkistan/dto/CreditDto.java
@@ -0,0 +1,27 @@
+package com.muratkistan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreditDto {
+
+    private String id;
+
+    private String identityNumber;
+
+    private double creditLimit;
+
+    private Boolean status;
+
+    public CreditDto(boolean status) {
+        this.status = status;
+    }
+}
--- credit-service/src/main/java/com/muratkistan/dto/UserDto.java
+++ credit-service/src/main/java/com/muratkistan/dto/UserDto.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,23 +13,18 @@ public class UserDto {
 
     private Long id;
 
-    @Column(name = "identity_number",unique = true)
-    @NotNull
+
     private String identityNumber;
 
-    @Column(name = "first_name")
-    @NotNull
+
     private String firstName;
 
-    @Column(name="last_name")
-    @NotNull
+
     private String lastName;
 
-    @Column(name = "monthly_salary")
-    @NotNull
+
     private double monthlySalary;
 
-    @Column(name = "phone_number",unique = true)
-    @NotNull
+
     private String phoneNumber;
 }
