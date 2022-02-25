b2393e0e1d43ba596e6b61673f1b4832c40ef470
muratkistan <muratkistan@gmail.com>
2022-02-25 19:19:20 +0300

    Added custom exception and handler class

---
+++ credit-service/src/main/java/com/muratkistan/exception/handler/GenericResponseError.java
@@ -0,0 +1,40 @@
+package com.muratkistan.exception.handler;
+
+//import com.muratkistan.custom.ApiError;
+import com.muratkistan.custom.ApiError;
+import com.muratkistan.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GenericResponseError {
+
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map> handleNotfoundException(NotFoundException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException exception){
+        Map<String,String> validationErrors = new HashMap<>();
+        ApiError error = new ApiError(400,"Validation Error","/users/add");
+        for (FieldError fieldError :  exception.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        error.setValidationErrors(validationErrors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+    }
+}
