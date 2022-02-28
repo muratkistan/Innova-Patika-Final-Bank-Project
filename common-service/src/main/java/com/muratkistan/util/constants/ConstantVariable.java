11aad55bcc458fdb36c18a8d10e8bf8be407ab71
muratkistan <muratkistan@gmail.com>
2022-02-28 07:03:59 +0300

    Added constant variable class to common-service module and refactoring variables

---
+++ common-service/src/main/java/com/muratkistan/util/constants/ConstantVariable.java
@@ -0,0 +1,10 @@
+package com.muratkistan.util.constants;
+
+public class ConstantVariable {
+
+    public static final String CHECK_USER_FROM_USER_SERVICE_PATH = "http://USER-SERVICE/users/exists/";
+    public static final String ADD_USER_TO_USER_SERVICE_PATH = "http://USER-SERVICE/users/add";
+    public static final Integer MULTIPLIER =4;
+    public static final Integer SMALL_CREDIT=10000;
+    public static final Integer MIDDLE_CREDIT=2000;
+}
