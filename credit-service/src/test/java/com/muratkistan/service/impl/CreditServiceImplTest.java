07ffcef0c277023d5800720a95f95249f953676f
muratkistan <muratkistan@gmail.com>
2022-02-28 06:31:56 +0300

    Added new test method to CreditServiceTestImpl

--- credit-service/src/test/java/com/muratkistan/service/impl/CreditServiceImplTest.java
+++ credit-service/src/test/java/com/muratkistan/service/impl/CreditServiceImplTest.java
@@ -1,21 +1,29 @@
 package com.muratkistan.service.impl;
 
 import com.muratkistan.dto.CreditDto;
+import com.muratkistan.dto.CreditScoreDto;
+import com.muratkistan.dto.UserDto;
 import com.muratkistan.model.Credit;
+import com.muratkistan.model.CreditScore;
+import com.muratkistan.model.mapper.CreditMapper;
+import com.muratkistan.model.mapper.CreditScoreMapper;
 import com.muratkistan.repository.CreditRepository;
 import com.muratkistan.service.abstracts.CreditScoreService;
 import org.junit.Assert;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -36,6 +44,11 @@ class CreditServiceImplTest {
 
     @InjectMocks
     private CreditServiceImpl creditService;
+
+    @BeforeEach
+    void setup() {
+        creditService = new CreditServiceImpl(creditRepository,creditScoreService, new ModelMapper(),new  RestTemplate());
+    }
 
 
 
@@ -60,39 +73,43 @@ class CreditServiceImplTest {
         verify(creditRepository).findAll();
     }
 
+    @Test
+    void addScore(){
+        Credit credit = Credit.builder().id("1111")
+                .identityNumber("1111").creditLimit(3000).status(true).build();
 
-//    @Test
-//    void getCredits_successful(){
-//
-//        //init step
-//        Credit expectedCredit = new Credit("3","546",10000,true);
-//
-////        stub - when step
-//        Optional<Credit> expectedOptionalCredit = (Optional<Credit>) Optional.of(expectedCredit);
-//        when(creditRepository.findByIdentityNumber("1111")).thenReturn(expectedOptionalCredit);
-//
-//        //then step
-//        CreditDto actualCreditDto = creditService.findCreditByIdentityNumber("546");
-//
-//        //valid step
-//        assertEquals(expectedCredit, actualCreditDto);
-//    }
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//    @Test
-//    void addCredit(){
-//        Credit expectedCredit = new Credit("1111","11111111111",10000,true);
-//        Credit credit2 = new Credit("2222","22222222222",20000,true);
-//        // stub - when
-//        when(creditRepository.save(expectedCredit)).thenReturn(expectedCredit);
-//
-//        // then
-//        creditService.addCredit(modelMapper.map(expectedCredit,CreditDto.class));
-//        Optional<Credit> byId = creditRepository.findById("1111");
-////
-//        Assert.assertEquals(expectedCredit, byId);
-//
-//        verify(creditRepository, times(1)).save(expectedCredit);
-//    }
+
+        CreditDto creditDto = CreditMapper.entityToDto(credit);
+
+        when(creditRepository.save(Mockito.any(Credit.class))).thenReturn(credit);
+
+        CreditDto savedCredit = creditService.addCredit(creditDto);
+
+        Assert.assertEquals(savedCredit.getIdentityNumber(),credit.getIdentityNumber());
+        Assert.assertEquals(savedCredit.getId(),credit.getId());
+
+    }
+
+    @Test
+    void getByIdentityNumber(){
+
+        Credit  credit = Credit.builder().id("1111")
+                .identityNumber("1111")
+                .creditLimit(10000).status(true).build();
+
+
+
+
+        when(creditRepository.findByIdentityNumber("1111")).thenReturn(Optional.of(credit));
+
+        CreditDto creditDto  = creditService.findCreditByIdentityNumber(credit.getIdentityNumber());
+
+        Assert.assertEquals(creditDto.getIdentityNumber(),credit.getIdentityNumber());
+
+    }
+
+
+
 
 
 
